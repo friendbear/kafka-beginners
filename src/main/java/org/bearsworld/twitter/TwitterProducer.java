41a0201d7469c59ab12bf5df8567ee8adb16d206
friendbear <friendbear@users.noreply.github.com>
2021-11-18 18:09:06 +0900

    TwitterProducer

---
+++ src/main/java/org/bearsworld/twitter/TwitterProducer.java
@@ -0,0 +1,25 @@
+package org.bearsworld.twitter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwitterProducer {
+
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+    public TwitterProducer() {}
+
+    public void run() {
+
+        logger.info("Setup");
+
+        // create a twitter client
+
+        // create a kafka producer
+
+        // loop to send tweets to kafka
+        logger.info("Stop Applications.");
+    }
+    public static void main(String[] argv) {
+
+    }
+}
