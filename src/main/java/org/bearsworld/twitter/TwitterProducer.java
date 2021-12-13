58fd31b038d1d61b07ec3ccb0aac2dba3403f704
friendbear <friendbear@users.noreply.github.com>
2021-12-13 22:49:14 +0900

    remove warning.

--- src/main/java/org/bearsworld/twitter/TwitterProducer.java
+++ src/main/java/org/bearsworld/twitter/TwitterProducer.java
@@ -4,20 +4,15 @@ import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
 
 import java.util.List;
 import java.util.Properties;
@@ -25,7 +20,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.System.getProperty;
 
 public class TwitterProducer {
 
@@ -39,8 +33,8 @@ public class TwitterProducer {
         logger.info("Setup");
 
         // create a twitter client
-        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+        // Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream
+        var msgQueue = new LinkedBlockingQueue<String>(1000);
         var client = createTwitterClient(msgQueue);
 
         client.connect();
@@ -69,13 +63,10 @@ public class TwitterProducer {
                 client.stop();
             }
             if (msg != null) {
-                kafkaProducer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
-                    @Override
-                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                kafkaProducer.send(new ProducerRecord<>("twitter_tweets", null, msg), (recordMetadata, e) -> {
 
-                        if (e != null) {
-                            logger.error("Something bad happened...", e);
-                        }
+                    if (e != null) {
+                        logger.error("Something bad happened...", e);
                     }
                 });
                 logger.info(msg);
@@ -87,7 +78,7 @@ public class TwitterProducer {
 
     public Client createTwitterClient(BlockingQueue<String> msgQueue) {
 
-        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        // Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth)
         var hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         var hosebirdEndpoint = new StatusesFilterEndpoint();
         hosebirdEndpoint.trackTerms(terms);
@@ -118,8 +109,7 @@ public class TwitterProducer {
                 .endpoint(hosebirdEndpoint)
                 .processor(new StringDelimitedProcessor(msgQueue));
 
-        Client hosebirdClient = builder.build();
-        return hosebirdClient;
+        return builder.build();
 
     }
     public KafkaProducer<String, String> createKafkaProducer() {
@@ -144,7 +134,7 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32 KB batch size
 
         //create the producer
-        return new KafkaProducer<String, String>(properties);
+        return new KafkaProducer<>(properties);
     }
 
     public static void main(String[] argv) {
