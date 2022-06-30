42a3d05768a5ead174a754b78f7b6c2e1845d5dd
T Kumagai <4956799+friendbear@users.noreply.github.com>
2022-07-01 05:07:24 +0900

    set high throughput producer configs

--- kafka-producer-wikimedia/src/main/java/bearsworld/kafkka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/bearsworld/kafkka/wikimedia/WikimediaChangesProducer.java
@@ -23,11 +23,16 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
         //create the producer
+
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
-
 
         String topic = "wikimedia.recentchange";
 
